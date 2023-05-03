f9f11b49c7b8971d9df79d9ca9a23c5d2898b187
vagner santos <vagner131417@gmail.com>
2023-05-03 16:27:07 -0300

    feat: create pages home history header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from "react-router-dom";
+
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>oi</h1>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
-  )
+  );
 }
