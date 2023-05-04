a22610eed1ff99e9153116e36801c1c048356315
vagner santos <vagner131417@gmail.com>
2023-05-04 15:37:40 -0300

    feat: create header menu add phosphor

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { HeaderContainer } from "./styles";
+
+export function Header() {
+  return(
+    <HeaderContainer>
+      <span>logo</span>
+      <nav>
+        <a href="">
+          timer
+        </a>
+        <a href="">
+          history
+        </a>
+      </nav>
+    </HeaderContainer>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,8 @@
+
 export function Home() {
   return(
-    <h1>Home</h1>
+    <div>
+      <h1>Home</h1>
+    </div>
   )
 }
