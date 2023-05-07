05a0328e811f5d0143fe5a00912f627f37a7562f
vagner santos <vagner131417@gmail.com>
2023-05-06 23:15:28 -0300

    feat: add table history

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,43 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
+
+export function History() {
+  return(
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluidos</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluidos</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluidos</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
---
+++ src/pages/History/styles.ts
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  h1{
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['white']}
+  }
+`;
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      background-color: ${(props) => props.theme['white']};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme['red-400']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background: rgb(255 255 255 / 17%);
+      border-top: 4px solid ${(props) => props.theme['red-400']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`;
